497708008ab2683d51c7084a0d2450e1bfe35512
guilherme <gui11_3@hotmail.com>
2019-04-28 21:26:39 -0300

    Criação de post usando rest template

---
+++ src/main/java/br/com/devdojo/javaclient/JavaClientDao.java
@@ -0,0 +1,49 @@
+package br.com.devdojo.javaclient;
+
+import br.com.devdojo.model.PageableResponse;
+import br.com.devdojo.model.Student;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+public class JavaClientDao {
+
+    private RestTemplate restTemplate = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v1/protected/students")
+            .basicAuthorization("toyo", "devdojo").build();
+
+
+    private RestTemplate restTemplateAdmin = new RestTemplateBuilder()
+            .rootUri("http://localhost:8080/v1/admin/students")
+            .basicAuthorization("toyo", "devdojo").build();
+
+
+    public Student findById(Long id) {
+        return restTemplate.getForObject("/{id}", Student.class, id);
+    }
+
+    public List<Student> listAll() {
+        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
+                new ParameterizedTypeReference<PageableResponse<Student>>() {
+                });
+
+        return exchange.getBody().getContent();
+    }
+
+    public Student save(Student student) {
+        ResponseEntity<Student> exchangePost = restTemplateAdmin.
+                exchange("/", HttpMethod.POST, new HttpEntity<>(student, createJsonHeader()), Student.class);
+
+        return exchangePost.getBody();
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+
+}
