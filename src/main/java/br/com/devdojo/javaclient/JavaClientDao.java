7fe7bdcd3a83c365c3df61281b295b2e98d55f81
Guilherme César Medeiros <guilherme.medeiros@terceiros.calcard.com.br>
2019-04-29 08:48:44 -0300

    Criando o handler

--- src/main/java/br/com/devdojo/javaclient/JavaClientDao.java
+++ src/main/java/br/com/devdojo/javaclient/JavaClientDao.java
@@ -1,5 +1,6 @@
 package br.com.devdojo.javaclient;
 
+import br.com.devdojo.handler.RestResponseExceptionHandler;
 import br.com.devdojo.model.PageableResponse;
 import br.com.devdojo.model.Student;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -17,11 +18,14 @@ public class JavaClientDao {
 
     private RestTemplate restTemplate = new RestTemplateBuilder()
             .rootUri("http://localhost:8080/v1/protected/students")
-            .basicAuthorization("user", "123456").build();
+            .basicAuthorization("user", "123456")
+            .errorHandler(new RestResponseExceptionHandler())
+            .build();
 
 
     private RestTemplate restTemplateAdmin = new RestTemplateBuilder()
             .rootUri("http://localhost:8080/v1/admin/students")
+            .errorHandler(new RestResponseExceptionHandler())
             .basicAuthorization("admin ", "123456").build();
 
     public Student findById(Long id) {
